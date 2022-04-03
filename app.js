b6e17e1202e0e7b6ae09da49c2e0357dffb5c5d6
sunandbean <sunbin.vincent.kim@gmail.com>
2022-04-03 14:18:16 +0900

    array

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
-const amIFat = null;
-let something;
-console.log(something)
-something = "a"
-console.log(something)
+const daysOfWeek = [ "mon", "tue", "wed", "thu", "fri", "sat" ]
+
+// Get Item from Array
+console.log(daysOfWeek)
+console.log(daysOfWeek[5])
+
+// Add one more day to the array
+daysOfWeek.push("sun")
+console.log(daysOfWeek)
+console.log(daysOfWeek[6])
+
+daysOfWeek[0] = "test"
+console.log(daysOfWeek)
