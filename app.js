59b9f2a5a8dee296d1b821c8cd8a025f4bfc9f69
sunandbean <sunbin.vincent.kim@gmail.com>
2022-04-03 17:35:27 +0900

    change the color

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
-const title = document.querySelector(".hello h1");
+const title = document.querySelector("div.hello:first-child h1");
+
+console.dir(title)
 
 title.innerText = "Hello"
+
+title.style.color = "blue"
