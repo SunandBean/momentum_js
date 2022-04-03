e077b22fdf3d57802b1b661d69f91ba94f1b8c75
sunandbean <sunbin.vincent.kim@gmail.com>
2022-04-03 17:45:03 +0900

    first event handliere

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
 const title = document.querySelector("div.hello:first-child h1");
 
+
+console.dir(title)
+
 function handleTitleClick() {
+    if (title.style.color == "blue"){
+        title.style.color = "red"
+    } else {
+        title.style.color = "blue"
+    }
+    
     console.log("title was clicked!")
 }
 
 title.addEventListener("click", handleTitleClick)
+
+console.dir(title)
