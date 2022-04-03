24d9c44d653bf95821ed341579e77be55e0c0d6f
sunandbean <sunbin.vincent.kim@gmail.com>
2022-04-03 13:38:07 +0900

    boolean, null and undefined

--- app.js
+++ app.js
@@ -1,20 +1,5 @@
-let a = 10;
-let b = 5;
-let myName = "nico";
-
-console.log(a + b);
-console.log(a - b);
-console.log(a * b);
-console.log(a / b);
-console.log("hello " + myName);
-
-console.log("value is changed")
-a = 15;
-b = 2;
-myName = "Sunbin"
-
-console.log(a + b);
-console.log(a - b);
-console.log(a * b);
-console.log(a / b);
-console.log("hello " + myName);
+const amIFat = null;
+let something;
+console.log(something)
+something = "a"
+console.log(something)
