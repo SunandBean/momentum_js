978d4e9dea7795ac40c8e865b3bf189f448a23b7
sunandbean <sunbin.vincent.kim@gmail.com>
2022-04-03 17:49:05 +0900

    add more events

--- app.js
+++ app.js
@@ -1,18 +1,24 @@
 const title = document.querySelector("div.hello:first-child h1");
 
-
-console.dir(title)
-
 function handleTitleClick() {
-    if (title.style.color == "blue"){
+    if (title.style.color == "blue") {
         title.style.color = "red"
     } else {
         title.style.color = "blue"
     }
-    
-    console.log("title was clicked!")
+
+    console.log("title was clicked!");
 }
 
-title.addEventListener("click", handleTitleClick)
+function handleMouseEnter() {
+    title.innerText = "Mouse is here!";
+}
 
+function handleMouseLeave() {
+    title.innerText = "Mouse is gone!";
+}
+
+title.addEventListener("click", handleTitleClick);
+title.addEventListener("mouseenter", handleMouseEnter);
+title.addEventListener("mouseleave", handleMouseLeave);
 console.dir(title)
